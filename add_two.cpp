59179644535bf830417551b109bc53db95f03e60
Ashvani Singh <146087438+Ashvani-2000@users.noreply.github.com>
2025-03-18 20:10:49 +0530

    Add files via upload

---
+++ add_two.cpp
@@ -0,0 +1,12 @@
+//Adddition of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition of two no is :"<<c;
+    return 0;
+}
